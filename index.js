2db3aa06da2f6a6f1580a34241701b2349b82532
Prasoon <prasoonsoni.work@gmail.com>
2022-05-24 22:46:33 +0530

    Database Connection Done 👍

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const connectToDatabase = require('./db/db')
+connectToDatabase()
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
