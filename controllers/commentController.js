22dfc41d571c1d7fdc019338fe80302510b34443
Prasoon Soni <prasoonsoni03@gmail.com>
2022-07-23 23:11:45 +0530

    Added route for deleting comment.

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -9,6 +9,7 @@ const addComment = async (req, res) => {
         const comment = req.body.comment
         const comment_time = Date.now()
         const data = {
+            _id: new ObjectId(),
             user_id: user_id,
             comment: comment,
             comment_time: comment_time,
@@ -30,4 +31,35 @@ const addComment = async (req, res) => {
     }
 }
 
-module.exports = { addComment }
+const deleteComment = async (req, res) => {
+    try {
+        const post_id = new ObjectId(req.params.id)
+        const comment_id = new ObjectId(req.params.comment_id)
+        const user_id = new ObjectId(req.user.user_id)
+        const comment = await Posts.findOne({
+            'posts._id': post_id,
+            'posts.comments._id': comment_id,
+            'posts.comments.user_id': user_id,
+        })
+        if (!comment) {
+            return res.json({ success: false, message: 'Comment not found.' })
+        }
+        const deleteComment = await Posts.updateOne(
+            { 'posts._id': post_id, 'posts.comments._id': comment_id, 'posts.comments.user_id': user_id },
+            { $pull: { 'posts.$.comments': { _id: comment_id } } }
+        )
+        if (!deleteComment.acknowledged) {
+            return res.json({ success: false, message: 'Error deleting comment.' })
+        }
+        res.json({ success: true, message: 'Comment Deleted Successfully.' })
+    } catch (error) {
+        console.log(error.message)
+        res.json({
+            success: false,
+            message: 'Some internal server error occured.',
+        })
+    }
+}
+
+
+module.exports = { addComment, deleteComment }
--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -4,5 +4,6 @@ const fetchuser = require('../middleware/fetchuser')
 const commentController = require('../controllers/commentController')
 
 router.post('/comment/:id', fetchuser, commentController.addComment)
+router.delete('/comment/:id/:comment_id', fetchuser, commentController.deleteComment)
 
 module.exports = router
